4ef811374239cc5ea2a9c25b3ae0d696bf50e532
Gaye Bulut <gayegul@gmail.com>
2015-12-20 11:23:04 -0800

    add function

---
+++ minDepthBinaryTree.js
@@ -0,0 +1,21 @@
+var minDepth = function(root) {
+    if(!root) return 0;
+    var depth = 1;
+    var rightDepth;
+    var leftDepth;
+    if(!root.left && !root.right) return depth;
+    else if(!root.left && root.right) {
+        rightDepth = minDepth(root.right);
+        depth += rightDepth;
+    }
+    else if(root.left && !root.right) {
+        leftDepth = minDepth(root.left);
+        depth += leftDepth;
+    }
+    else {
+        leftDepth = minDepth(root.left);
+        rightDepth = minDepth(root.right);
+        depth += leftDepth < rightDepth ? leftDepth : rightDepth;
+    }
+    return depth;
+};
