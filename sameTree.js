241fbe7026816d3e93dca20d6fb7b05ef11e261c
Gaye Bulut <gayegul@gmail.com>
2016-03-16 11:24:33 -0700

    minor fix

--- sameTree.js
+++ sameTree.js
@@ -10,13 +10,12 @@
  * @param {TreeNode} q
  * @return {boolean}
  */
- 
-var isSameTree = function(p, q) {
-    if(!p && !q) return true;
-    else if((p && !q) || (!p && q)) return false;
-    else if((p && q) && (p.val !== q.val)) return false;
-    else if((p.right && !q.right) || (!p.right && q.right)) return false;
-    else if((p.left && !q.left) || (!p.left && q.left)) return false;
 
-    return (isSameTree(p.right, q.right) && isSameTree(p.left, q.left));
+var isSameTree = function(p, q) {
+  if(!p && !q) return true;
+  else if((p && !q) || (!p && q)) return false;
+  else if((p && q) && (p.val !== q.val)) return false;
+  else if((p.right && !q.right) || (!p.right && q.right)) return false;
+  else if((p.left && !q.left) || (!p.left && q.left)) return false;
+  return (isSameTree(p.right, q.right) && isSameTree(p.left, q.left));
 };
