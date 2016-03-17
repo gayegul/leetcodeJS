6f9a16e7d356d779872c48aec17f1e86338005c7
Gaye Bulut <gayegul@gmail.com>
2016-03-17 00:25:30 -0700

    minor fix

--- lowestCommonAncestor.js
+++ lowestCommonAncestor.js
@@ -11,11 +11,11 @@
  * @param {TreeNode} q
  * @return {TreeNode}
  */
- 
+
 var lowestCommonAncestor = function(root, p, q) {
-    if(!root || !p || !q) return null;
-    else if(root === p || root === q) return root;
-    else if((p.val <= root.val && q.val > root.val) || (p.val > root.val && q.val <= root.val)) return root;
-    else if(p.val <= root.val && q.val <= root.val) return lowestCommonAncestor(root.left, p, q);
-    else return lowestCommonAncestor(root.right, p, q);
+  if(!root || !p || !q) return null;
+  else if(root === p || root === q) return root;
+  else if((p.val <= root.val && q.val > root.val) || (p.val > root.val && q.val <= root.val)) return root;
+  else if(p.val <= root.val && q.val <= root.val) return lowestCommonAncestor(root.left, p, q);
+  else return lowestCommonAncestor(root.right, p, q);
 };
