0c4af523bc5c35dd882c95c08a18cbcb532cf432
Gaye Bulut <gayegul@gmail.com>
2016-05-16 08:38:22 -0700

    update solution

--- happyNumber.js
+++ happyNumber.js
@@ -4,20 +4,19 @@
  */
  
 var isHappy = function(n) {
-    var holder = {};
-
-    while(true) {
-        var nums = n.toString().split("");
-        var total = 0;
-        for(var i = 0; i < nums.length; i++) {
-            total += Math.pow(nums[i], 2);
-        }
-
-        if(total === 1) return true;
-
-        if(holder[total]) return false;
-        else holder[total] = true;
-
-        n = total;
+  var holder = {};
+  var result = 0;
+  var number = n;
+  while(!holder[result]) {
+    while(number !== 0) {
+      result += Math.pow((number % 10), 2);
+      number = Math.floor(number / 10);
     }
+    if(result === 1) return true;
+    if(holder[result]) return false;
+    holder[result] = 1;
+    number = result;
+    result = 0;
+  }
+  return false;
 };
