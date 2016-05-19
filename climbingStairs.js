451bc8f20acc90852acc03f49a394a362e448aae
Gaye Bulut <gayegul@gmail.com>
2016-05-19 01:02:37 -0700

    update solution

--- climbingStairs.js
+++ climbingStairs.js
@@ -4,21 +4,9 @@
  */
 
 var climbStairs = function(n) {
-    if(n <= 0) return 0;
-
-    var climb1 = 1;
-    if(n === 1) return climb1;
-
-    var climb2 = 2;
-    if(n === 2) return climb2;
-    
-    var stairs = climb1 + climb2;
-
-    for(var i = 3; i <= n; i++) {
-        climb = climb1 + climb2;
-        climb1 = climb2;
-        climb2 = climb;
-    }
-
-    return climb;
+  var steps = [0, 1, 2];
+  for(var i = 3; i <= n; i++) {
+    steps.push(steps[i - 2] + steps[i - 1]);
+  }
+  return steps[n];
 };
