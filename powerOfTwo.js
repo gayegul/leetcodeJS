d86f7a0b30183aef119033987db8f2c25efbd1bc
Gaye Bulut <gayegul@gmail.com>
2016-05-22 00:47:34 -0700

    add solution

--- powerOfTwo.js
+++ powerOfTwo.js
@@ -3,7 +3,25 @@
  * @return {boolean}
  */
 
+// using the biggest integer that is a power of 2
 var isPowerOfTwo = function(n) {
   if(n > 0 && (Math.pow(2, 30) % n === 0)) return true;
   return false;
+};
+
+// using math
+var isPowerOfTwo = function(n) {
+  return ((Math.log10(n) / Math.log10(2)) % 1 === 0);
+};
+
+// using a loop
+var isPowerOfTwo = function(n) {
+  if(n <= 0) return false;
+
+  while(n >= 2) {
+    if(n % 2 !== 0) return false;
+    n /= 2;
+  }
+
+  return n % 2 === 1;
 };
