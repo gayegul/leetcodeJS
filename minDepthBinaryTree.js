cba2d3574ab7560f7dd3877916dd154385dc0d80
Gaye Bulut <gayegul@gmail.com>
2016-05-29 12:15:36 -0700

    update solution

--- minDepthBinaryTree.js
+++ minDepthBinaryTree.js
@@ -1,22 +1,27 @@
-//Recursively
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
 var minDepth = function(root) {
-    if(!root) return 0;
-    var depth = 1;
-    var rightDepth;
-    var leftDepth;
-    if(!root.left && !root.right) return depth;
-    else if(!root.left && root.right) {
-        rightDepth = minDepth(root.right);
-        depth += rightDepth;
-    }
-    else if(root.left && !root.right) {
-        leftDepth = minDepth(root.left);
-        depth += leftDepth;
-    }
-    else {
-        leftDepth = minDepth(root.left);
-        rightDepth = minDepth(root.right);
-        depth += leftDepth < rightDepth ? leftDepth : rightDepth;
-    }
-    return depth;
+  return getDepth(root, 0);
 };
+
+function getDepth(root, result) {
+  if(!root) return result;
+
+  var left = getDepth(root.left, result) + 1;
+  var right = getDepth(root.right, result) + 1;
+
+  if(!root.right) return left;
+  if(!root.left) return right;
+
+  return Math.min(left, right);
+}
