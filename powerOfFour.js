ce1ec70b34556e1ec3e930e8853ceea119d67aee
Gaye Bulut <gayegul@gmail.com>
2016-06-01 00:34:44 -0700

    add solution

---
+++ powerOfFour.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+
+var isPowerOfFour = function(num) {
+  while(num > 1) {
+    if(num % 4 !== 0) return false;
+    num /= 4;
+  }
+
+  return num === 1;
+};
