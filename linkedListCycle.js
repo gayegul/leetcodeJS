a0bc1bb6d124a6b7fbe6e2b88aef8b3964616c27
Gaye Bulut <gayegul@gmail.com>
2016-06-15 10:29:58 -0700

    update solution

--- linkedListCycle.js
+++ linkedListCycle.js
@@ -1,15 +1,44 @@
-//Given a linked list, determine if it has a cycle in it
-//Floyd's cycle-finding algorithm || The Tortoise and Hare Algorithm
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
 
 var hasCycle = function(head) {
-    if(!head) return false;
-    var slow = head;
-    var fast = head;
-    while(fast.next) {
+  if(!head || !head.next) return false;
+  var slow = head;
+  var fast = head.next.next;
+  while(fast && fast.next) {
+    if(slow === fast) return true;
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return false;
+};
+
+// return node where cycle meets
+var detectCycle = function(head) {
+  if(head === null) return null;
+  var slow = head;
+  var fast = head;
+  while(fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if(slow === fast) {
+      slow = head;
+      while(true) {
+        if(slow === fast) return slow;
         slow = slow.next;
-        fast = fast.next.next;
-        if(!fast) return false;
-        if(slow === fast) return true;
+        fast = fast.next;
+      }
     }
-    return false;
+  }
+  return null;
 };
