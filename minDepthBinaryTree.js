414acfd5502cc60a3a9f71dd8dd36dc4dd84efa4
Gaye Bulut <gayegul@gmail.com>
2016-06-16 22:55:51 -0700

    update solution

--- minDepthBinaryTree.js
+++ minDepthBinaryTree.js
@@ -11,17 +11,19 @@
  */
 
 var minDepth = function(root) {
-  return getDepth(root, 0);
+    return getDepth(root, 0);
 };
 
 function getDepth(root, result) {
-  if(!root) return result;
-
-  var left = getDepth(root.left, result) + 1;
-  var right = getDepth(root.right, result) + 1;
-
-  if(!root.right) return left;
-  if(!root.left) return right;
-
-  return Math.min(left, right);
+    if(!root) return result;
+    var left = getDepth(root.left, result) + 1;
+    var right = getDepth(root.right, result) + 1;
+    // if you have both children you can pick min depth
+    if(root.left && root.right) return Math.min(left, right);
+    // if you don't have both children you should pick the existing depth
+    // you cannot pick 0 for the non existing child, that path is closed
+    else {
+        if(!root.right) return left;
+        if(!root.left) return right;
+    }
 }
